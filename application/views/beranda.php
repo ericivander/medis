286116933c8ee74c871f6fd4b5810a8f92014a3b
santensuru <djuned.ong@gmail.com>
2015-10-17 00:03:48 +0700

    editing beranda view

--- application/views/beranda.php
+++ application/views/beranda.php
@@ -13,6 +13,80 @@
 		</div>
         <!-- /.col-lg-12 -->
     </div>
+	<!-- /.row -->
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dokter</div>
+				<div class="panel-body">
+					<a class="btn btn-default" role="button" onclick="check(true);">
+						<i class="fa fa-check fa-fw"></i>
+						Check All
+					</a>
+					<a class="btn btn-default" role="button" onclick="uncheck(true);">
+						<i class="fa fa-close fa-fw"></i>
+						Uncheck All
+					</a>
+				</div>
+				<div class="panel-body" style="height:100px; overflow:auto">
+				<?php
+				if($doctor != null)
+				{
+					foreach($doctor as $row)
+					{
+				?>
+					<div class="checkbox">
+						<label>
+							<input class="dokter_cb" id="<?php echo $row->id;?>" type="checkbox" onclick="toogle('<?php echo $row->id;?>');" checked> <?php echo $row->id;?>
+						</label>
+					</div>
+				<?php
+					}
+				}
+				?>
+                </div>
+                <!-- /.panel-body -->
+            </div>
+            <!-- /.panel -->
+        </div>
+        <!-- /.col-lg-6 -->
+		<div class="row">
+        <div class="col-lg-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">Kabupaten/Kota</div>
+				<div class="panel-body">
+					<a class="btn btn-default" role="button" onclick="check(false);">
+						<i class="fa fa-check fa-fw"></i>
+						Check All
+					</a>
+					<a class="btn btn-default" role="button" onclick="uncheck(false);">
+						<i class="fa fa-close fa-fw"></i>
+						Uncheck All
+					</a>
+				</div>
+				<div class="panel-body" style="height:100px; overflow:auto">
+				<?php
+				if($city != null)
+				{
+					foreach($city as $row)
+					{
+				?>
+					<div class="checkbox">
+						<label>
+							<input class="kabkota_cb" id="<?php echo $row->id;?>" type="checkbox" onclick="toogle('<?php echo $row->id;?>');" checked> <?php echo $row->id;?>
+						</label>
+					</div>
+				<?php
+					}
+				}
+				?>
+                </div>
+                <!-- /.panel-body -->
+            </div>
+            <!-- /.panel -->
+        </div>
+		<!-- /.col-lg-6 -->
+    </div>
     <!-- /.row -->
     <div class="row">
         <div class="col-lg-12">
@@ -28,7 +102,7 @@
 										{
 											foreach($city as $row)
 											{
-												echo "<th>";
+												echo '<th class="'.$row->id.'">';
 												echo $row->id;
 												echo "</th>";
 											}
@@ -44,7 +118,7 @@
 									{
 										foreach($doctor as $row)
 										{
-											echo "<tr>";
+											echo '<tr class="'.$row->id.'">';
 											echo "<th>";
 											echo $row->id;
 											echo "</th>";
@@ -56,11 +130,11 @@
 													$result = $assignment[$crow][$ccol++];
 													if($result == 999)
 													{
-														echo '<td style="background-color:#CC0000; color:white">';
+														echo '<td class="'.$row->id.' '.$col->id.'" style="background-color:#CC0000; color:white">';
 													}
 													else
 													{
-														echo '<td style="background-color:#33CC33">';
+														echo '<td class="'.$row->id.' '.$col->id.'" style="background-color:#33CC33">';
 													}
 													echo $result;
 													echo "</td>";
@@ -85,3 +159,73 @@
     <!-- /.row -->
 </div>
 <!-- /#page-wrapper -->
+<script type="text/javascript">
+	function toogle(IDname)
+	{
+		//alert(IDname);
+		if (document.getElementById(IDname).checked) 
+		{
+			//alert("set " + IDname + " visible");
+			var cells = document.getElementsByClassName(IDname), i;
+			$("." + IDname).show();
+			for (var i = 0; i < cells.length; i ++)
+			{
+				//cells[i].style.visibility = 'visible';
+			}
+		}
+		else
+		{
+			//alert("set " + IDname + " hidden");
+			var cells = document.getElementsByClassName(IDname), i;
+			$("." + IDname).hide();
+			for (var i = 0; i < cells.length; i ++)
+			{
+				//cells[i].style.visibility = 'hidden';
+			}
+		}
+	}
+	
+	function check(flag)
+	{
+		if(flag)
+		{
+			var checkboxes = document.getElementsByClassName('dokter_cb'), i;
+			for (var i = 0; i < checkboxes.length; i ++)
+			{
+				checkboxes[i].checked = true;
+				toogle(checkboxes[i].id);
+			}
+		}
+		else
+		{
+			var checkboxes = document.getElementsByClassName('kabkota_cb'), i;
+			for (var i = 0; i < checkboxes.length; i ++)
+			{
+				checkboxes[i].checked = true;
+				toogle(checkboxes[i].id);
+			}
+		}
+	}
+	
+	function uncheck(flag)
+	{
+		if(flag)
+		{
+			var checkboxes = document.getElementsByClassName('dokter_cb'), i;
+			for (var i = 0; i < checkboxes.length; i ++)
+			{
+				checkboxes[i].checked = false;
+				toogle(checkboxes[i].id);
+			}
+		}
+		else
+		{
+			var checkboxes = document.getElementsByClassName('kabkota_cb'), i;
+			for (var i = 0; i < checkboxes.length; i ++)
+			{
+				checkboxes[i].checked = false;
+				toogle(checkboxes[i].id);
+			}
+		}
+	}
+</script>
